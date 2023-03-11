ca8ae43fff9e957c8a44a98535c9cd5ad00c13ef
fazaido <nelson.srelvas@gmail.com>
2023-03-11 14:05:52 -0300

    criptografando senha

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-
-import express from 'express'
+import express from 'express';
+import { pool } from './mysql';
+import { v4 as uuidv4 } from 'uuid'
+import { hash } from 'bcrypt'
 
 const app = express();
-
-//resolver possíveis problemas no json, senao n recebe json
 app.use(express.json());
 
 // endpoint local: http://localhost:4000/
@@ -14,13 +14,37 @@ app.use(express.json());
 // app.put();
 // app.delete();
 
-app.get('/', (request, response) => {
-    // response.send('Vocẽ acessou o servidor');
-    response.json({ nome: 'Nelson', sobrenome: 'Relvas', idade: 37 });
+app.get('/create-user', (request, response) => {
+    const { name, email, password } = request.body;
+    console.log(name, email, password);
+    pool.getConnection((err: any, connection: any) => {
+
+        const idGenerated = uuidv4();
+        hash(password, 10, (err, passwordHash) => {
+
+            if (err) {
+                return response.status(500).json(err);
+            }
+
+            console.log(idGenerated);
+            console.log(passwordHash);
+
+            connection.query(
+                'INSERT INTO users (user_id, name, email, password) VALUES (?,?,?,?)',
+                [idGenerated, name, email, passwordHash],
+                (error: any, result: any, fields: any) => {
+                    if (error) {
+                        response.status(400).json(error);
+                    } else {
+                        response.status(200).json({ sucess: true });
+                    }
+                }
+            );
+        });
+    })
 })
 
 app.get('/users', (request, response) => {
-    // response.send('Vocẽ acessou o servidor');
     console.log('iniciando1');
     response.json([{ nome: 'Nelson', sobrenome: 'Relvas', idade: 37 },
     { nome: 'Maria', sobrenome: 'Relvas', idade: 60 },
@@ -31,9 +55,7 @@ app.get('/users', (request, response) => {
 
 
 app.post('/userdata/:id/:email', (request, response) => {
-    console.log('iniciando 222');
     console.log(request.query);
-    // console.log(request.route);
 
     console.log('-------------------------------');
     console.log(request.headers);
@@ -44,13 +66,7 @@ app.post('/userdata/:id/:email', (request, response) => {
     console.log('parametro', request.params.id);
     console.log('parametro', request.params.email);
 
-    // console.log(request.body);
-    // console.log(request.body.nome);
-    // console.log(request.body.sobrenome);
-    // console.log(request.body.idade);
-    // response.send('ok')  //status ok
     response.status(200).json({ sucess: true });
-    console.log('finalizando 222');
 })
 
 
